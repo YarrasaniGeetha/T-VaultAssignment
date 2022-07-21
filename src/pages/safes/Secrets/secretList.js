d5b8b04fc83f727417da71c35bc099856b83dd36
Yarrasani Geetha <yarrasanigeetha@piktorlabs.com>
2022-07-21 14:47:29 +0530

    Updated

--- src/pages/safes/Secrets/secretList.js
+++ src/pages/safes/Secrets/secretList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux/es/exports";
 import secretCard from "./secret.svg";
 import deleteSecret from "./delete.svg";
